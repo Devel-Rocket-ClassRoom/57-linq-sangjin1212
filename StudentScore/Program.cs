8eb4c93581a4fd3a0759d055b02ba5e2431a6996
YunSangJin <sangjin411@gmail.com>
2026-03-17 18:04:16 +0900

    상진

--- StudentScore/Program.cs
+++ StudentScore/Program.cs
@@ -2,5 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+List<Student> students = new List<Student>
+{
+    new Student("김철수","수학",85),
+    new Student("김철수","영어",78),
+    new Student("이영희","수학",92),
+    new Student("이영희","영어",88),
+    new Student("박민수","수학",76),
+    new Student("박민수","영어",82),
+    new Student("정지은","수학",95),
+    new Student("정지은","영어",91)
+};
---
+++ StudentScore/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Student
+{ 
+    public string Name { get; private set; }
+    public string Subject { get; private set; }
+    public int Score { get; private set; }
+
+    public Student(string name, string subject, int score)
+    {
+        Name = name;
+        Subject = subject;
+        Score = score;
+    }
+}
